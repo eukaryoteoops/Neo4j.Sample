bdf5146ca1f6967e7b0ebd03b7a353856c7f570b
TonyChang <tony.chang@run-cloud.com.tw>
2019-12-12 19:08:00 +0800

    Init.

---
+++ Neo4j.Sample/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+using Newtonsoft.Json;
+
+namespace Neo4j.Sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly IGraphClient _neo4j;
+
+        public ValuesController(IGraphClient neo4j)
+        {
+            _neo4j = neo4j;
+        }
+
+        [HttpGet("level/from/{from}")]
+        public IActionResult GetLevel([FromRoute]string from)
+        {
+            //create if not exist
+            _neo4j.Cypher.Merge("(a:Person{name:\"root\"})-[:Child]->(m:Person{name:\"level1\"})")
+                .Merge("(m)-[:Child]->(n:Person{name:\"level2\"})")
+                .Merge("(m)-[:Child]->(o:Person{name:\"level2-1\"})")
+                .Merge("(n)-[:Child]->(p:Person{name:\"level3\"})")
+                .Merge("(p)-[:Child]->(q:Person{name:\"level4\"})").ExecuteWithoutResults();
+            //find all node start with "{from}"
+            var users = _neo4j.Cypher
+                .OptionalMatch("(n:Person)-[*]->(p:Person)")
+                .Where((Person n) => n.Name == from)
+                .Return(p => p.As<Person>());
+            var self = _neo4j.Cypher
+                .OptionalMatch("(n:Person)")
+                .Where((Person n) => n.Name == from)
+                .Return(n => n.As<Person>());
+            var a = users.Results;
+            var result = users.Results.First() == null ? self.Results : self.Results.Union(users.Results);
+            return Ok(result);
+        }
+
+    }
+
+    public class Person
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Neo4j.Sample/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Neo4jClient;
+
+namespace Neo4j.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGraphClient>(context =>
+            {
+                var graphClient = new GraphClient(new Uri("http://localhost:7474/db/data"));
+                graphClient.Connect();
+                return graphClient;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
